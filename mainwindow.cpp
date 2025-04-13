b493d78d1ce8f548440f08b3bc34c56b961d03d3
Quinlin <102311011+qnobletaylor@users.noreply.github.com>
2025-04-13 12:15:46 -0400

    created addition btn action

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,15 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+int firstNum, secondNum, result;
+
+void MainWindow::on_btnAdd_clicked()
+{
+    firstNum = ui->txtFirstNum->text().toInt();
+    secondNum = ui->txtSecondNum->text().toInt();
+    result = firstNum+secondNum;
+
+    ui->txtResult->setText(QString::number(result));
+}
+
